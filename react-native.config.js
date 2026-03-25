1d48e5a5b525b5fcf422d957746083232d021639
ziadtarek112 <ziad.elnagdy@gmail.com>
2026-03-25 23:10:35 +0200

    init

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  dependencies: {
+    'react-native-worklets': {
+      root: require.resolve('react-native-worklets/package.json').replace('/package.json', ''),
+    },
+  },
+};
